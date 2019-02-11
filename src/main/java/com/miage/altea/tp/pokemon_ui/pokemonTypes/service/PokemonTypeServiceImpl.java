6ab49a5aed077596959be7a12b5e4e54e81da8c9
SAUVALLE Damien <damien.sauvalle.etu@univ-lille.fr>
2019-02-11 11:19:37 +0100

    update service

---
+++ src/main/java/com/miage/altea/tp/pokemon_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
@@ -0,0 +1,33 @@
+package com.miage.altea.tp.pokemon_ui.pokemonTypes.service;
+
+import com.miage.altea.tp.pokemon_ui.pokemonTypes.bo.PokemonType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class PokemonTypeServiceImpl implements PokemonTypeService {
+
+    private RestTemplate restTemplate;
+
+    private String url;
+
+    public List<PokemonType> listPokemonsTypes() {
+
+        PokemonType[] forObject = restTemplate.getForObject(this.url, PokemonType[].class);
+
+        return Arrays.asList(forObject);
+    }
+
+    @Autowired
+    void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    void setPokemonTypeServiceUrl(String pokemonServiceUrl) {
+        this.url = pokemonServiceUrl + "/pokemon-types";
+    }
+}
