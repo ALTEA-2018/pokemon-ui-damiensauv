a7f8ae8a18db4853e4ea974897e8330259ff0966
SAUVALLE Damien <damien.sauvalle.etu@univ-lille.fr>
2019-03-25 11:31:59 +0100

    wip

--- src/main/java/com/miage/altea/tp/pokemon_ui/config/RestConfiguration.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/config/RestConfiguration.java
@@ -3,11 +3,7 @@ package com.miage.altea.tp.pokemon_ui.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 public class RestConfiguration {
--- src/main/java/com/miage/altea/tp/pokemon_ui/pokemonTypes/bo/Trainer.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/pokemonTypes/bo/Trainer.java
@@ -1,13 +1,7 @@
 package com.miage.altea.tp.pokemon_ui.pokemonTypes.bo;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-
-@Entity
 public class Trainer {
 
-
-    @Column
     private String password;
 
 
